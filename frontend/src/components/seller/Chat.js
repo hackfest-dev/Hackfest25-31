2cc371037653a20f2f378fbe51a0877a34bae08f
Nayuraikar <nayu.raikar@gmail.com>
2025-04-18 19:56:08 +0530

    adding the Seller Dashboard files

---
+++ frontend/src/components/seller/Chat.js
@@ -0,0 +1,9 @@
+export default function Chat() {
+    return (
+      <div style={{ padding: '20px' }}>
+        <h2> Chat with Our Buyers</h2>
+        <p>Chat feature coming soon!</p>
+      </div>
+    );
+  }
+  
