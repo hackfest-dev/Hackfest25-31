99fb9e1f00e8d6c643030eef903135b2ae3086f9
Sanjana-1905 <prasasthi.s@gmail.com>
2025-04-18 21:30:43 +0530

    adding routes in app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import BuyerSignup from './components/auth/BuyerSignup';
+import BuyerLogin from './components/auth/BuyerLogin';
+import SellerSignup from './components/auth/SellerSignup';
+import SellerLogin from './components/auth/SellerLogin';
+import DeliverySignup from './components/auth/DeliverySignup';
+import DeliveryLogin from './components/auth/DeliveryLogin';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/buyer/signup" element={<BuyerSignup />} />
+        <Route path="/buyer/login" element={<BuyerLogin />} />
+        <Route path="/seller/signup" element={<SellerSignup />} />
+        <Route path="/seller/login" element={<SellerLogin />} />
+        <Route path="/delivery/login" element={<DeliveryLogin />} />
+        <Route path="/delivery/signup" element={<DeliverySignup />} />
+        <Route path="/seller/dashboard" element={
+  <>
+    <SellerNav />
+    <SellerHome />
+  </>
+} />
+</Routes>
+    </Router>
   );
 }
 
